b51f7d272e5e38019922a2d1bc88bb6c704d8ef0
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2021-10-30 15:11:10 -0400

    Adding consts and random generator to create original board

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 
 
-function App() {
+const width = 8;
+const height = 8;
+const candyColors = [
+        "blue",
+        "green",
+        "orange",
+        "pruple",
+        "red",
+        "yellow"
+];
+
+const App = () => {
+
+  const createBoard = () => {
+    for (let i = 0; i < width * height; i++) {
+        const randomColor = candyColors[Math.floor(Math.randon() * candyColors.length)];
+    }
+  }
+
   return (
     <div></div>
   );
